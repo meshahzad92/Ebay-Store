1e2959c2ab567ae8774bd3fff1eb3546227de48f
unknown <umairimran627@gmail.com>
2024-04-26 02:01:36 +0500

    mn

--- databasee.py
+++ databasee.py
@@ -6,7 +6,12 @@ import sqlite3
 import sqlite3
 
 class Users:
-    
+    def __init__(self):
+        self.userName = ""
+        self.userEmail = ""
+        self.userPassword = ""
+        self.userContact = ""
+
     def insertUser(userPassword, userEmail, userName, userContact):
         try:
             conn = sqlite3.connect('database.db')
@@ -18,7 +23,18 @@ class Users:
             return 'Error occurred: {}'.format(e)
         finally:
             conn.close()
-
+    def updateUser(user_email, new_name, new_password, new_contact,currentEmail):
+        try:
+            conn = sqlite3.connect('database.db')
+            cursor = conn.cursor()
+            cursor.execute("UPDATE users SET userPassword = ?, userEmail = ?, userName = ?, userContact = ? WHERE userEmail = ?",
+               (new_password, user_email, new_name, new_contact, currentEmail))
+            conn.commit()
+            return 'Data updated successfully'
+        except sqlite3.Error as e:
+            return 'Error occurred: {}'.format(e)
+        finally:
+            conn.close()
 
     def getUsers(query, params=None):
         try:
@@ -35,3 +51,79 @@ class Users:
             conn.close()
 
             
+
+
+class addressess():
+    def __init__(self):
+        self.userAddressID = 0
+        self.userContact = ""
+        self.userCountry = ""
+        self.userCity = ""
+        self.userStreet = ""
+        self.userZipCode = ""
+        self.userEmail = ""
+    def addressess(self, userContact="", userCountry="", userCity="", userStreet="", userZipCode="", userEmail=""):
+        
+        self.userContact = userContact
+        self.userCountry = userCountry
+        self.userCity = userCity
+        self.userStreet = userStreet
+        self.userZipCode = userZipCode
+        self.userEmail = userEmail
+    def updateAddress(userContact, userCountry, userCity, userStreet, userZipCode, userEmail,new_address_id):
+        try:
+            conn = sqlite3.connect('database.db')
+            cursor = conn.cursor()
+            print("I'm in updateAddress")  # Ensure function is being called
+            cursor.execute("UPDATE addresses SET userContact = ?, userCountry = ?, userCity = ?, userStreet = ?, userZipCode = ? WHERE userEmail = ? and userAddressID = ?", (userContact, userCountry, userCity, userStreet, userZipCode, userEmail, new_address_id))
+            conn.commit()
+            return 'Data updated successfully'
+        except sqlite3.Error as e:
+            return 'Error occurred: {}'.format(e)
+        finally:
+            conn.close()
+
+    def insertAddress(userContact, userCountry, userCity, userStreet, userZipCode, userEmail):
+        try:
+            conn = sqlite3.connect('database.db')
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO addresses (userContact, userCountry, userCity, userStreet, userZipCode, userEmail) VALUES (?, ?, ?, ?, ?, ?)",
+                       (userContact, userCountry, userCity, userStreet, userZipCode, userEmail))
+            conn.commit()
+            return 'Data inserted successfully'
+        except sqlite3.Error as e:
+            return 'Error occurred: {}'.format(e)
+        finally:
+            conn.close()
+
+
+
+def user_exist(userEmail,query,params=None):
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute(query,params)
+    user = cursor.fetchall()
+    conn.close()
+    if user:
+        return True
+    return False
+
+
+def is_user_logged_in(session):
+    if 'userEmail' in session:
+        return True
+    return False
+
+def execute_query(query, params=None):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    if params:
+        cursor.execute(query, params)
+    else:
+        cursor.execute(query)
+    conn.commit()
+    rows = cursor.fetchall()  # Fetch result rows
+    conn.close()
+    return rows
+
--- main.py
+++ main.py
@@ -8,36 +8,147 @@ from flask import url_for
 from flask import session
 
 app = Flask(__name__)
+app.secret_key = '03025202775Abc$'  # replace 'your secret key' with your actual secret key
+
+@app.route("/")
+def home():
+    return render_template("malefashion-master/home.html")
+
+@app.route("/userProfile",methods=["GET","POST"])
+def userProfile():
+    if request.method=="GET":
+        if databasee.is_user_logged_in(session)==False:
+            return redirect(url_for("login"))
+
+        return render_template("malefashion-master/userProfile.html")
+    return render_template("malefashion-master/userProfile.html")
+
+
+
+
+@app.route("/addAddress",methods=["GET","POST"])
+def addAddress():
+
+    if request.method=="POST":
+
+        databasee.addressess.insertAddress(request.form["userContact"],request.form["userCountry"],request.form["userCity"],request.form["userStreet"],request.form["userZipCode"],session["userEmail"])
+        
+        return redirect(url_for("home"))
+    elif request.method=="GET":
+        if databasee.is_user_logged_in(session)==False:
+            session['referrer'] = request.referrer
+            return redirect(url_for("login"))
+        return render_template("malefashion-master/addAddress.html")
+
+
+
+    return render_template("malefashion-master/addAddress.html")
+
+
+
+
+@app.route("/updateAddress", methods=["GET", "POST"])
+def updateAddress():
+    if request.method == "GET":
+        addresses = databasee.execute_query("SELECT * FROM addresses WHERE userEmail=?", (session["userEmail"],))
+        return render_template("malefashion-master/updateAddress.html", addresses=addresses)
+    
+    elif request.method == "POST":
+        # Retrieve the address ID from the form data
+        address_id = request.form.get("addressId")
+        
+        # Use the address ID to fetch the address details from the database
+        result = databasee.execute_query("SELECT * FROM addresses WHERE userAddressId=?", (address_id,))
+        print(result)
+        if result:
+            # Instantiate the address object
+            addressObject = databasee.addressess()
+            
+            # Assign values to addressObject attributes
+            addressObject.userContact = result[0][1]
+            addressObject.userCountry = result[0][2]
+            addressObject.userCity = result[0][3]
+            addressObject.userStreet = result[0][4]
+            addressObject.userZipCode = result[0][5]
+            addressObject.userEmail = result[0][6]
+            addressObject.userAddressId = result[0][0]
+            return render_template("malefashion-master/addressess.html", userObject=addressObject)
+        else:
+            return "Address not found"
+
+@app.route("/deleteAddress", methods=["GET", "POST"])
+def deleteAddress():
+    if request.method == "GET":
+        addresses=databasee.execute_query("select * from addresses where userEmail=?",(session["userEmail"],))
+        return render_template("malefashion-master/deleteAddress.html",addresses=addresses)
+    elif request.method == "POST":
+        # Retrieve the address ID from the form data
+        address_id = request.form.get("addressId")
+        
+        # Use the address ID to fetch the address details from the database
+        databasee.execute_query("delete  FROM addresses WHERE userAddressId=?", (address_id,))
+        return redirect(url_for("home"))
+
+
+@app.route("/addPaymentMethod")
+def addPaymentMethod():
+    return render_template("malefashion-master/addPaymentMethod.html")
+@app.route("/updatePaymentMethod")
+def updatePaymentMethod():
+    return render_template("malefashion-master/updatePaymentMethod.html")
+@app.route("/deletePaymentMethod")  
+def deletePaymentMethod():
+    return render_template("malefashion-master/deletePaymentMethod.html")
+
+
+@app.route("/addressManagement")
+def addressManagement():
+    return render_template("malefashion-master/addressManagement.html")
+
+@app.route("/paymentManagement")
+def paymentManagement():
+    return render_template("malefashion-master/paymentManagement.html")
+@app.route("/updatePersonalInfo")
+def updatePersonalInfo():
+    return render_template("malefashion-master/updatePersonalInfo.html")
 @app.route("/login", methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        
+        session['referrer'] = request.referrer
         userEmail = request.form['userEmail']
         userPassword = request.form['userPassword']
-        
+        print(userEmail,userPassword)
         # Query to retrieve user based on email
         query = "SELECT * FROM users WHERE userEmail = ?"
        
         # Retrieve user(s) from the database based on email
         users = databasee.Users.getUsers(query, (userEmail,))
-        
+
         # Check if any user matches the provided email
         if users:
+            print("users is not empty here")
+            print(userPassword)
             # Check if the password matches
-            if users["userPassword"] == userPassword:
+            if  users[0][1]== userPassword:
+                print("inside storing in session")
                 # Storing user information in session
-                session["userName"] = users["userName"]
-                session["userEmail"] = users["userEmail"]
-                
-                return render_template("malefashion-master/shop.html")  # Redirect to the dashboard route
+                session["userName"] = users[0][3]
+                session["userEmail"] = users[0][2]
+                print("successfully logged in   ")
+                # if 'referrer' in session:
+                #     referrer = session.pop('referrer')  # Get and remove the referrer URL from session
+                #     return redirect(referrer)
+                return redirect(url_for("home"))  # Redirect to the dashboard route
             else:
+                print("password dont matched")
                 # Password is incorrect, show login page with an error message
                 return render_template('malefashion-master/login.html', error="Invalid email or password")
         else:
+         print("user dont exist ")
             # User doesn't exist, show login page with an error message
          return   redirect(url_for('signUp', error="Invalid email or password"))
 
-    else:
+    elif request.method == 'GET':
         # If it's a GET request, render the login page
        return render_template("malefashion-master/login.html")
 
@@ -59,41 +170,103 @@ def signUp():
         if request.form["confirmPassword"]!=userPassword:
             print("i ma in first if")
             return  redirect(url_for("signUp"))
-        users=databasee.Users.getUsers("insert into * FROM users WHERE userEmail = ?", (userEmail,))
+        users=databasee.Users.getUsers("select * from users WHERE userEmail = ?", (userEmail,))
         if users:
             print("i am in second if")
-            return   redirect(url_for('login', error="User already exists"))
+            return redirect(url_for('login', error="User already exists"))
         else:
             print("i am in else")
+            print("successfully inserted")
             databasee.Users.insertUser(userPassword, userEmail, userName, userContact)
+            databasee.addressess.insertAddress("","","","","","",userEmail)
+            if 'referrer' in session:
+                referrer = session.pop('referrer')  # Get and remove the referrer URL from session
+                return redirect(referrer)
             return redirect(url_for('login'))
     else:
         return render_template('malefashion-master/signUp.html')
 
 
-@app.route("/personelInfo")
-def personelInfo():
+@app.route("/personalInfo", methods=["GET", "POST"])
+def personalInfo():
+    currentEmail=""
     
-    userName.text=session["userName"]
-    userEmail.text=session["userEmail"]
-    userEmail=session["userEmail"]
-    user=databasee.Users.getUsers("select * from users where userEmail=?",userEmail)
-    userName.text=user["userName"]
-    userEmail.text=user["userEmail"]
-    userPassword.text=user["userPassword"]
-    userCOntact.text=user["userPassword"]
+    if request.method == "GET":
+        # Fetch user's information from the database based on their email
+        if 'userEmail' not in session:
+            return redirect(url_for('login'))
+        user_email = session["userEmail"] 
+        
+        currentEmail=user_email
+        user = databasee.Users.getUsers("SELECT * FROM users WHERE userEmail=?", (user_email,))
+        # Check if user exists
+        if user:
+            userObject = databasee.Users()
+            userObject.userName = user[0][3]
+            userObject.userEmail = user[0][2]
+            userObject.userPassword = user[0][1]
+            userObject.userContact = user[0][4]
+            return render_template('malefashion-master/personelInfo.html', userObject=userObject)
+        else:
+            return "User not found"
+    elif request.method == "POST":
+        # Retrieve edited information from the form
+        new_name = request.form["userName"]
+        new_password = request.form["userPassword"]
+        new_contact = request.form["userContact"]
+        # Update the database with the edited information
+        user_email = session["userEmail"]  
+        databasee.Users.updateUser(user_email, new_name, new_password, new_contact,session["userEmail"])
+        # Check if there's a referrer URL stored in the session
+        if 'referrer' in session:
+            referrer = session.pop('referrer')  # Get and remove the referrer URL from session
+            return redirect(referrer)  # Redirect the user back to the referrer URL
+        else:
+            return redirect(url_for("login"))  # Redirect to login if no referrer URL is found
 
-    return render_template("malefashion-master\personelInfo.html")
-
-@app.route("/userAddressess")
+@app.route("/userAddressess", methods=["GET", "POST"]  )
 def addressess():
-     userEmail=session["userEmail"]
-    user=databasee.Users.getUsers("select * from users where userEmail=?",userEmail)
-    userCOntact.text=user["userCOntact"]
-usercountry.text=user["userCountry"]
-
-userCity=user["userCity"]
-userStreet=user["userStreet"]
+    print("hey i am in addressess")
+    if request.method == "GET":
+        
+        if  databasee.is_user_logged_in(session)==False:
+             return  redirect(url_for('login'))
+        user_email = session["userEmail"]
+        user = databasee.execute_query("SELECT * FROM addresses WHERE userEmail=?", (session["userEmail"],))
+        userAddress=databasee.addressess()
+        print(user)
+        if user:
+            userAddress.addressess(user[0][1],user[0][2],user[0][3],user[0][4],user[0][5],user[0][6])
+            print(userAddress.userContact,userAddress.userCountry,userAddress.userCity,userAddress.userStreet,userAddress.userZipCode,userAddress.userEmail)
+            return render_template('malefashion-master/addressess.html', userObject=userAddress)
+        userAddress.userEmail=session["userEmail"]
+        return render_template('malefashion-master/addressess.html', userObject=userAddress)
+    elif request.method == "POST":
+            print("i am in post")
+    # # Retrieve edited information from the form
+        
+            
+            new_contact = request.form["userContact"]
+            new_country = request.form["userCountry"]
+            new_city = request.form["userCity"]
+            new_street = request.form["userStreet"]
+            new_zip_code = request.form["userZipCode"]
+            new_address_id = request.form["userAddressId"]
+            databasee.addressess.updateAddress( new_contact, new_country, new_city, new_street, new_zip_code,session["userEmail"],new_address_id)
+            
+    # # Check if there's a referrer URL stored in the session
+            return redirect(url_for("shop"))
+      
     
+
+@app.route('/clear')
+def clear():
+    session.clear()
+    return redirect(url_for('home'))
+@app.route("/shop", methods=["GET", "POST"])
+def shop():
+    if request.method == "GET":
+        return render_template('malefashion-master/shop.html')
+
 if __name__ == '__main__':
     app.run()
